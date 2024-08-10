1ce535e1cb43720e6d752a08052e08bee66f210c
saurabh-kumar1233 <kumarsaurabh6661@gmail.com>
2024-08-10 23:09:57 +0530

    arr_sum.java

---
+++ arr_sum.java
@@ -0,0 +1,12 @@
+public class arr_sum {
+    public static void main(String args[])
+    {
+        int sum=0;
+      int arr[]={1,2,3,4,5};
+      for (int i=0;i<=arr.length;i++)
+      {
+        sum=sum+i;
+        System.out.println(sum);
+      }
+    }
+}
