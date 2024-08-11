d6811961a987b3c17af3395a03a8794695c8f2f9
saurabh-kumar1233 <kumarsaurabh6661@gmail.com>
2024-08-11 21:13:54 +0530

    loop commit

---
+++ Matrix_print.java
@@ -0,0 +1,17 @@
+public class Matrix_print {
+    public static void main(String args[])
+    {
+      
+        int arr1[][]={{1,2,3,4,5},{7,9,8,5,6}};
+        
+        for (int i=0;i<=4;i++)
+        {
+            for (int j=0;j<=4;j++)
+            {
+                System.out.print(arr1[i][j]);
+            }
+           System.out.println();
+        }
+    }
+}
+
---
+++ While_loop.java
@@ -0,0 +1,11 @@
+public class While_loop {
+    public static void main(String args[])
+    {
+        int number=5;
+        while(number==5)
+        {
+            System.out.println("saurabh your have a number grater than 0");
+            
+        }
+    }
+}
